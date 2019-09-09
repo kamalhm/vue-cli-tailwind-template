2cd7c1e6fc916fbd17170aee773a3d782342fdcd
Kamal Mahmud <kamalhm1997@gmail.com>
2019-09-09 17:20:21 +0700

    moved postcss config to separate file, add @tailwind on App.vue, initialized tailwind config

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="text-6xl font-bold text-green-900">Hello from tailwind</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@tailwind base;
+@tailwind components;
+@tailwind utilities; 
 </style>
